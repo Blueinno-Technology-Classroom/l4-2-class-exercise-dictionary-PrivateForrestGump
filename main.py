1b643e0bbf2ea7adb17a0206ae8168e03b8cc120
PrivateForrestGump <forrestleung58@gmail.com>
2024-03-09 10:32:03 +0800

    eee

--- main.py
+++ main.py
@@ -4,20 +4,61 @@ Q1:
 '''
 
 # TODO: Write your code here
+sample_dict = {
+   "name": "Hermione",
+   "marks": {
+       "transfiguration": 90,
+       "charms": 98,
+       "potions": 98,
+       "history of magic": 92,
+   }
+}
 
+print(sample_dict['marks']['history of magic'])
 ##################################################
 '''
 Q2:
 '''
 
 # TODO: Write your code here
+sample_dict = {
+    'emp1': {'name': 'Jhon', 'salary': 7500},
+    'emp2': {'name': 'Emma', 'salary': 8000},
+    'emp3': {'name': 'Brad', 'salary': 500}
+}
 
+sample_dict['emp3']['salary'] = 8500
+
+print(sample_dict)
 ##################################################
 '''
 Q3:
 '''
 
 # TODO: Write your code here
+mystery_pokemon = {
+     "height": 15,
+     "weight": 405,
+     "moves": [
+         {
+             "name": "lick",
+             "level_learned_at": 0
+         },
+         {
+             "name": "nightmare",
+             "level_learned_at": 0
+         },
+         {
+             "name": "curse",
+             "level_learned_at": 16
+         }
+     ]
+}
+
+move_list = mystery_pokemon['moves']
+print(move_list[2]["level_learned_at"])
+
+
 
 ##################################################
 '''
